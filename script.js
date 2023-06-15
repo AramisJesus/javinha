85a57ac016684408438a0609923c96b1b97dd191
AramisJesus <aramis.oi@hotmail.com>
2023-06-14 21:11:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function alo(){
+    alert("alo!")
+}
+
+function alonome(nome){
+   // alert("alo" + nome) //concatenação de strings
+    alert( "alo ${nome}. Seja Bem vindo ao curso!")
+}
+
+function saudacao(){
+    const nome = document.getElementById
+    if (nome=== "") {
+        alert("digite um nome")
+        const nome = document.getElementById ("nome").focus()
+
+    }
+    else{
+        alert (' ${nome} seja bem vindo . ')
+
+    }
+
+    function mousecima(){
+        alert('O mouse esta aqui')
+    }
+    function mousefora(){
+        alert('O mouse saiu!')
+    }
+  
+}
